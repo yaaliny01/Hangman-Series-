74d47e6982bb6f698757f9fd0b93b3b474ecc494
yaaliny01 <yaaliny.sg01@gmail.com>
2024-07-25 00:31:20 +0530

    Add files via upload

---
+++ Hangman series.py
@@ -0,0 +1,32 @@
+# PROJECT_3
+# HANGMAN_GAME
+
+import random
+import hangman_stages
+import word_file
+#word_list = ["apple", "beautiful", "potato"]
+lives = 6
+chosen_word = random.choice(word_file.words)
+print(chosen_word)
+display = []
+for i in range(len(chosen_word)): # 1 2 3 4 5
+    display += '_'
+print(display)
+game_over = False
+while not game_over:
+    guessed_letter = input("Guess a letter: ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guessed_letter:
+            display[position] == guessed_letter
+    print(display)
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+    if '_' not in display:
+        game_over = True
+        print("You win!!!")
+    print(hangman_stages.stages[lives])
+print("Hope you enjoyed it!!!")
